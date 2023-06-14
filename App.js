fe0c355180efa2d3f3911ad888a45e925cf19d27
Herberth Kagiya <tayki.heberth@gmail.com>
2023-06-14 10:47:21 -0300

    Image placed in background with gradient

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StyleSheet } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { StyleSheet, ImageBackground } from 'react-native';
 import {LinearGradient} from "expo-linear-gradient";
+
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
@@ -8,7 +9,14 @@ export default function App() {
       colors={["#4e0329" ,"#ddb52f"]}
       style={styles.rootScreen}>
 
-      <StartGameScreen/>
+      <ImageBackground 
+        source={require("./assets/images/background.png")}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}>
+
+        <StartGameScreen/>
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -16,5 +24,9 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1
+  },
+
+  backgroundImage: {
+    opacity: 0.15
   }
 });
