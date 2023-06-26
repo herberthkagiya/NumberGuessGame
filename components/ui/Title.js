c3649f3a0b07ab3ae776c6e5c32f953839a05549
Herberth Kagiya <tayki.heberth@gmail.com>
2023-06-26 10:02:11 -0300

    Fonts added and icons to minus and plus bottons

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,5 +1,4 @@
 import { Text, StyleSheet } from "react-native";
-import Colors from "../../util/colors";
 
 function Title({children}){
     return(
@@ -11,8 +10,8 @@ export default Title;
 
 const styles = StyleSheet.create({
     title: {
+        fontFamily: "open-sans-bold",
         fontSize: 24,
-        fontWeight: "bold",
         color: "white",
         textAlign: "center",
         borderWidth: 2,
